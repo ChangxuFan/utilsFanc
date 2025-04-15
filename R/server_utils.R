aad0ff1f50d8144b63646f4262504f02bdce81c5
fanc <changxu.fan@outlook.com>
2025-04-15 14:10:48 -0500

    upload WangHPC contents. 2025-04-15

--- R/server_utils.R
+++ R/server_utils.R
@@ -1,12 +1,17 @@
+# bash2ftp <- function(filename) {
+#   filename <- add.pwd(filename)
+#   user <- Sys.getenv("USER")
+#   
+#   ftp <- sub("^~", paste0("https://wangftp.wustl.edu/~", user), filename) %>%
+#     sub(paste0("/bar/", user), paste0("https://wangftp.wustl.edu/~", user), .) %>%
+#     sub(paste0("/scratch/", user, "/"), paste0("https://wangftp.wustl.edu/~", user, "/sth/"), .) %>% 
+# 	sub("/scratch/fanc/", "https://wangftp.wustl.edu/~cfan/sth/",.)
+#   return(ftp)
+# }
+
 bash2ftp <- function(filename) {
-  filename <- add.pwd(filename)
-  user <- Sys.getenv("USER")
-  
-  ftp <- sub("^~", paste0("https://wangftp.wustl.edu/~", user), filename) %>%
-    sub(paste0("/bar/", user), paste0("https://wangftp.wustl.edu/~", user), .) %>%
-    sub(paste0("/scratch/", user, "/"), paste0("https://wangftp.wustl.edu/~", user, "/sth/"), .) %>% 
-	sub("/scratch/fanc/", "https://wangftp.wustl.edu/~cfan/sth/",.)
-  return(ftp)
+  cmd <- paste0("~/R_for_bash/bf.R ", paste0(filename, collapse = " "))
+  ftp <- system(cmd, intern = T)
 }
 
 add.pwd <- function(filename) {
